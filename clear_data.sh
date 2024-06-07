0aa33249236cbdb1d3585fe791383f669bfb0d34
Edan Bainglass <edan.bainglass@gmail.com>
2024-06-07 07:22:59 +0200

    Update cleanup script

--- clear_data.sh
+++ clear_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -fr openbis/temp_uploads/* openbis/ro_crate
-rm -fr aiida/temp_uploads/* aiida/ro_crate
+rm -fr openbis/temp_uploads/* openbis/ro_crates/*
+rm -fr aiida/temp_uploads/* aiida/ro_crates/*
